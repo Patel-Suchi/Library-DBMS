c949da7a4bc522b1f43a4ac22bba25d4db90de68
Suchi Patel <81126387+Patel-Suchi@users.noreply.github.com>
2021-12-20 11:46:20 -0500

    Update drop_tables.sh

--- Unix Shell Implementation/drop_tables.sh
+++ Unix Shell Implementation/drop_tables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sqlplus64 "s352pate/Moon5009@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))"<<EOF
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
 DROP TABLE return_status;
 DROP TABLE issue_status;
