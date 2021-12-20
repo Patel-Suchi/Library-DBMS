0bd8d2513a9fc0de18eee471f3c9dc1e9ca9dbbd
Suchi Patel <81126387+Patel-Suchi@users.noreply.github.com>
2021-12-20 11:35:20 -0500

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sqlplus64 "s352pate/Moon5009@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
 SELECT
         isbn,
